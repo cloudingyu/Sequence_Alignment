38006f3a10ee3b27ad865ef46da6657ed91f6c64
cloudingyu <3245526833@qq.com>
2025-03-19 18:40:02 +0800

    3.19.18.39

--- build/CMakeFiles/lab1.dir/cmake_clean.cmake
+++ build/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -1,10 +1,12 @@
 file(REMOVE_RECURSE
-  "/Users/chenjane/Desktop/计算机科学与技术/算法设计与分析/Algorithm_design_and_analysis_project/lab1/bin/lab1"
-  "/Users/chenjane/Desktop/计算机科学与技术/算法设计与分析/Algorithm_design_and_analysis_project/lab1/bin/lab1.pdb"
-  "CMakeFiles/lab1.dir/lab1/src/HeadFile/repeat.cpp.o"
-  "CMakeFiles/lab1.dir/lab1/src/HeadFile/repeat.cpp.o.d"
-  "CMakeFiles/lab1.dir/lab1/src/lab1.cpp.o"
-  "CMakeFiles/lab1.dir/lab1/src/lab1.cpp.o.d"
+  "C:/ComputerScience/Algorithm_design_and_analysis_project/lab1/bin/lab1.exe"
+  "C:/ComputerScience/Algorithm_design_and_analysis_project/lab1/bin/lab1.exe.manifest"
+  "C:/ComputerScience/Algorithm_design_and_analysis_project/lab1/bin/lab1.pdb"
+  "CMakeFiles/lab1.dir/lab1/src/HeadFile/repeat.cpp.obj"
+  "CMakeFiles/lab1.dir/lab1/src/HeadFile/repeat.cpp.obj.d"
+  "CMakeFiles/lab1.dir/lab1/src/lab1.cpp.obj"
+  "CMakeFiles/lab1.dir/lab1/src/lab1.cpp.obj.d"
+  "liblab1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
