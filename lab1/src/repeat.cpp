806dab67376bd69bea4b97cf4d62b57b7d2b244b
cloudingyu <3245526833@qq.com>
2025-03-25 21:33:56 +0800

    3.25.21.33

--- lab1/src/repeat.cpp
+++ lab1/src/repeat.cpp
@@ -6,6 +6,11 @@
 #include <limits>
 using namespace std;
 
+// 参数: 维持, 新开, 反向维持, 反向新开
+const int alignScore = 5, newScore = -1, alignRevScore = 3, newRevScore = -5;
+
+const int MIN_INF = -2147483648;
+
 // 记录重复信息
 void Repeat_Segment::recordSegment(string const &seq, int loc, int len, int cnt, bool rev)
 {
@@ -50,33 +55,86 @@ void Repeat_Collection::analyzeRepeats(const string &reference, const string &qu
     vector<long long> refeRevHash = rollingRevHash(reference, kmer_size);
 
     // 动态规划数组初始化
-    vector<vector<int>> scoreMap(querLength, vector<int>(refeLength, 0));
+    vector<vector<long long>> scoreMap(querLength, vector<long long>(refeLength, MIN_INF));
     vector<vector<pair<int, int>>> prevMap(querLength, vector<pair<int, int>>(refeLength, {-1, -1}));
 
-    int reward = 1, penalty = -5;
+    // 记录第 i 个碱基对所对应的得分最大值
+    vector<pair<long long, pair<int, int>>> maxScoreMap(querLength, {MIN_INF, {0, 0}});
 
-    // 前一 query 碱基对应的最大得分及位置
-    int globalMaxScore = 0;
-    pair<int, int> globalMaxLoc = {-1, -1};
+    // 初始化 query 第 [0] 个碱基的匹配情况
+    scoreMap[0][0] = alignScore;
+    prevMap[0][0] = {-1, -1};
+    maxScoreMap[0] = {alignScore, {-1, -1}};
 
-    // 主循环,遍历每一个 query 串可以匹配的正向和反向 reference
+    // 主循环,遍历每一个 query 碱基可以匹配的正向和反向 reference
     for (int i = 1; i < querLength; i++)
         for (int j = 1; j < refeLength; j++)
         {
+            // 考虑正向匹配
+            if (querHash[i] == refeHash[j])
+            {
+                scoreMap[i][j] = scoreMap[i - 1][j - 1] + alignScore;
+                prevMap[i][j] = {i - 1, j - 1};
+
+                if (maxScoreMap[i - 1].first + alignScore + newScore > scoreMap[i][j])
+                {
+                    scoreMap[i][j] = maxScoreMap[i - 1].first + alignScore + newScore;
+                    prevMap[i][j] = maxScoreMap[i - 1].second;
+                }
+            }
+            /*
+            // 考虑反向匹配
+            else if (querHash[i] == refeRevHash[j])
+            {
+                scoreMap[i][j] = scoreMap[i - 1][j + 1] + newRevScore;
+                prevMap[i][j] = {i - 1, j + 1};
+
+                if (maxScoreMap[i - 1].first + alignRevScore + newRevScore > scoreMap[i][j])
+                {
+                    scoreMap[i][j] = maxScoreMap[i - 1].first + alignRevScore + newRevScore;
+                    prevMap[i][j] = maxScoreMap[i - 1].second;
+                }
+            }
+            */
+            if (maxScoreMap[i].first < scoreMap[i][j])
+                maxScoreMap[i] = {scoreMap[i][j], prevMap[i][j]};
         }
 
     cout << "equalMap" << endl;
     for (int i = refeLength - 1; i >= 0; i--)
     {
         for (int j = 0; j < querLength; j++)
-            cout << setw(3) << (bool)(refeHash[i] == querHash[j]) <<" ";
-        cout<<endl;
+            cout << setw(3) << (bool)(refeHash[i] == querHash[j]) << " ";
+        cout << endl;
     }
+
     cout << "scoreMap" << endl;
     for (int i = refeLength - 1; i >= 0; i--)
     {
         for (int j = 0; j < querLength; j++)
-            cout << setw(3) << scoreMap[j][i] << " ";
+            cout << setw(12) << scoreMap[j][i] << " ";
+        cout << endl;
+    }
+
+
+    for(int i=0;i<querLength;i++)
+        cout<<i<<" "<<maxScoreMap[i].first<<" "<<maxScoreMap[i].second.first<<" "<<maxScoreMap[i].second.second<<endl;
+
+
+    cout << "map" << endl;
+    vector<vector<int>> map(querLength, vector<int>(refeLength, 0));
+    for(int i=0;i<querLength-1;i++)
+        map[maxScoreMap[i+1].second.first][maxScoreMap[i+1].second.second] = 1;
+    map[querLength-1][refeLength-1] = 1;
+    for(int i = refeLength - 1; i >= 0; i--)
+    {
+        for(int j = 0; j < querLength; j++)
+        {
+            if(map[j][i] == 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
         cout << endl;
     }
     return;
